5878358f3e589b8af1be8ec412503e0826f03248
CC2OO2 <127530135+CC2OO2@users.noreply.github.com>
2023-04-11 22:56:39 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <audioio.h>
-
+#include <sstream>
+#include <stdio.h>
 
 int main(int argc, char *argv[]){
 	
@@ -16,16 +17,74 @@ int main(int argc, char *argv[]){
 	AudioReader dry(argv[2]);
         
         std::vector<double> src;
-        while(!wet.eof()){
-		src.push_back(wet.get());    
-        }
+	int i {0};
+	while(!wet.eof())
+	{
+		if(i>=ns)
+			vout.push_back(src.get());
+		i++;
+	}
         
         std::vector<double> training;
+        i=0;
         while(!dry.eof()){
-		training.push_back(dry.get());    
+        	if(i>=ns)
+			training.push_back(dry.get());
+		i++;    
         }
         
-        print(size(src) + " " + size(training))
+        std::cout<<size(src)<<" "<<size(training)<<'\n';
         
-        pclose
+        Fir1 fir(nc);
+        int fnum {1};
+        double sum {0);
+        
+        for(i=0;i<size(src);i++)
+        	sum = sum + src[i] *src[i];
+        double msi = sum / size(src);
+        
+        bool ok {true};
+        if(size(src)!=size(training))
+        {
+        	ok=false;
+        	std::cout<<"interference and source samples are different lengths"<<'\n';
+        }
+        
+        if(ok)
+        {
+        	const int trial=sr*2;
+        	std::cout<<"Pre-training..."<<'\n';
+        	
+        	if(trial<size(src))
+        		for(i=0;i<trial;i++)
+        			fir.lms_update(src[i]-fir.filter(training[i]));
+        	else
+        		for(i=0;i<size(src);i++)
+				fir.lms_update(src[i]-fir.filter(training[i]));
+				
+		std::cout<<"Processing..."<<'\n';        	
+        	
+        	std::vector<double> error;
+        	for(i=0;i<size(training);i++)
+        	{
+        		error.push_back(src[i] - fir.filter(training[i]));
+        		fir.lms_update(error[i]);
+        	}
+        	
+        	sum=0;
+        	for(i=0;i<size(error);i++)
+        		sum=sum+error[i]*error[i];
+        	double power_gain = sum/msi;
+        	std::cout<<"Power gain: "<<power_gain<<'\n';
+        	
+        	for(i=0;i<size(src);i++)
+        	{
+        		std::stringstream sso;
+        		sso << src[i] << std::endl;
+        		std::string cmdo("sox '" + argv[3] + "' -t wav -c 1 -");
+        		pipeo = popen(cmdo.c_str(), "r"); 
+        		fputs(sso.str().c_str(), pipeo);
+        	}
+        }
+        	
 }
